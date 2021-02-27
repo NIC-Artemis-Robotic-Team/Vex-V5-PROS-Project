5786406a8a89c080258d01c705205b244774a8a6
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-02-26 16:28:07 -0800

    added autonomous function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include "main.h"
 
+// Port Assignment
 #define LEFT_WHEELS_PORT 1
 #define RIGHT_WHEELS_PORT 10
 #define ARM_PORT 8
 #define CLAW_PORT 20
 
+
+
+
+// Tank Controls
 void opcontrol() {
 
 std::cout << "Tedst\n";
@@ -23,4 +28,20 @@ pros::Controller master (CONTROLLER_MASTER);
 		claw.move(master.get_analog(ANALOG_RIGHT_X));
 		pros::delay(2);
 	}
+}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() 
+{
+	// autonomous functions calls go here
 }
