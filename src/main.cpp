61be5840d1cdfe8e4352609d6b0f0fc20525ca55
Dan Blanchette <dlblanchette@students.nic.edu>
2021-03-04 17:08:39 -0800

    added a sample ultrasonic sensor routine for testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,21 @@
 #define CLAW_PORT 20
 
 
+// ultrasonic port assignment 
+/*NOTE: The PING wire (orange) must always be assigned to an odd numbered Port
+IE: 1,3,5,.... OR 'A','C','E'... 
+
+The ECHO wire (yellow) is then assigned to the next consecutive value  
+even number port*/
+
+// sample initialization for ports values can be modified
+#define ECHO 8 // Output - Send
+#define PING 9 // Input - Recieve
 
 
 // Tank Controls
-void opcontrol() {
+void opcontrol()
+{
 
 std::cout << "Tedst\n";
 
@@ -19,29 +30,19 @@ pros::Motor arm(ARM_PORT);
 pros::Motor claw(CLAW_PORT);
 pros::Motor right_wheels (RIGHT_WHEELS_PORT, true); // This reverses the motor
 pros::Controller master (CONTROLLER_MASTER);
+pros::ADIUltrasonic ultrasonic(PING,ECHO); // ultrasonic sensor
+	while (true)
+	{
+		// while the rover is greater than 100cm away from an object keep driving
+		while (ultrasonic.get_value() > 100 )
+		{
+			left_wheels.move(master.get_analog(ANALOG_LEFT_Y));
+			right_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
 
-	while (true) {
-		left_wheels.move(master.get_analog(ANALOG_LEFT_Y));
-		right_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
-
-		arm.move(master.get_analog(ANALOG_LEFT_X));
-		claw.move(master.get_analog(ANALOG_RIGHT_X));
-		pros::delay(2);
+			arm.move(master.get_analog(ANALOG_LEFT_X));
+			claw.move(master.get_analog(ANALOG_RIGHT_X));
+			pros::delay(2);
+		}
 	}
-}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() 
-{
-	// autonomous functions calls go here
+	
 }
