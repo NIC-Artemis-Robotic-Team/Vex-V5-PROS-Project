f9a9bb2ac52d108b3d51fbfb324bafad6770dc82
Dan-Blanchette <dlblanchette@students.nic.edu>
2021-03-06 12:50:13 -0800

    backup of ultrasonic test code

--- src/autoTest.cpp
+++ src/autoTest.cpp
@@ -7,52 +7,42 @@
 // #define CLAW_PORT 20
 
 
-// ultrasonic port assignment 
+// ultrasonic port assignment
 /*NOTE: The PING wire (orange) must always be assigned to an odd numbered Port
-IE: 1,3,5,.... OR 'A','C','E'... 
-
-The ECHO wire (yellow) is then assigned to the next consecutive value  
+IE: 1,3,5,.... OR 'A','C','E'...
+The ECHO wire (yellow) is then assigned to the next consecutive value
 even number port*/
 
 // sample initialization for ports values can be modified
-#define ECHO 8 // Output - Send
-#define PING 9 // Input - Recieve
+#define ECHO 'B'  // Output - Send
+#define PING 'A' // Input - Recieve
 
 
-// // Tank Controls
-// void opcontrol()
-// {
 
-// std::cout << "Tedst\n";
-
-// pros::Motor left_wheels (LEFT_WHEELS_PORT);
-// pros::Motor arm(ARM_PORT);
-// pros::Motor claw(CLAW_PORT);
-// pros::Motor right_wheels (RIGHT_WHEELS_PORT, true); // This reverses the motor
-// pros::Controller master (CONTROLLER_MASTER);
-// 	while (true)
-// 	{
-// 			left_wheels.move(master.get_analog(ANALOG_LEFT_Y));
-// 			right_wheels.move(master.get_analog(ANALOG_RIGHT_Y));
-
-// 			arm.move(master.get_analog(ANALOG_LEFT_X));
-// 			claw.move(master.get_analog(ANALOG_RIGHT_X));
-// 			pros::delay(2);
-// 	}
-// }
-
-void autonomous()
+// Tank Controls
+void opcontrol()
 {
-	pros::ADIUltrasonic ultrasonic(PING,ECHO); // ultrasonic sensor	
-	pros::Motor left_wheels(LEFT_WHEELS_PORT);
-	pros::Motor right_wheels(RIGHT_WHEELS_PORT);
 
-	while(utrasonic.get_value() > 100)
+std::cout << "Tedst\n";
+pros::ADIUltrasonic ultrasonic(PING,ECHO);
+pros::Motor left_wheels (LEFT_WHEELS_PORT);
+pros::Motor right_wheels (RIGHT_WHEELS_PORT, true); // This reverses the motor
+pros::Controller master (CONTROLLER_MASTER);
+	while (true)
 	{
-		left_wheels = 127;
-		right_wheels =127;
-		pros::delay(50);
+		int distance = ultrasonic.get_value();
+			if (distance > 200 || distance <= 0 )
+			{
+				left_wheels = master.get_analog(ANALOG_LEFT_Y);
+				right_wheels= master.get_analog(ANALOG_RIGHT_Y);
+				pros::delay(50);
+			}
+			else
+			{
+				left_wheels = 0;
+				right_wheels = 0;
+				left_wheels = -45;
+				right_wheels = -45;
+			}
 	}
-	left_wheels = 0;
-	right_wheels =0;
 }
